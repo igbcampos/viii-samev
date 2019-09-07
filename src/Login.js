6ae0daeca553355c21e2d1fbb4c3b31f0effdcb2
Francisco Jair <fjair123@ufpi.edu.br>
2019-09-07 10:33:01 -0300

    Tentado colocar um layout

--- src/Login.js
+++ src/Login.js
@@ -11,79 +11,91 @@ export default class Login extends Component {
 
     //Fazer login
     async login() {
-        <ActivityIndicator size='large'/>
+        <ActivityIndicator size='large' />
 
         await Firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
-        .then((user) => {
+            .then((user) => {
 
-            // user.user.updateProfile({
-            //     displayName: 'Gabriel Costa Campos',
-            //     photoURL: 'user',
-            // });
+                // user.user.updateProfile({
+                //     displayName: 'Gabriel Costa Campos',
+                //     photoURL: 'user',
+                // });
 
-            // user.user.updatePhoneNumber = '06487654321';
-            
-            if(user.user.photoURL == 'user') {
-                this.props.navigation.navigate('UserHome');
-            }
-            else if(user.user.photoURL == 'admin') {
-                this.props.navigation.navigate('AdminHome');
-            }
+                // user.user.updatePhoneNumber = '06487654321';
 
-            // Firebase.database().ref('/users/').once('value')
-            // .then((snapshot) => {
-            //     console.log(JSON.stringify(snapshot));
-            //     snapshot.val().map((data) => {
-            //         console.log(JSON.stringify(snapshot));
-            //         // if(data.email == user.email) {
-            //         //     if(data.bond == 'user') {
-            //         //         this.props.navigation.navigate('UserHome');
-            //         //     }
-            //         //     else if(data.bond == 'admin') {
-            //         //         this.props.navigation.navigate('AdminHome');
-            //         //     }
-            //         // }
-            //     });
-            // });
-        })
-        .catch((error) => {
-            if(error.code === 'auth/invalid-email') {
-                this.setState({ status: 'Esse não é um formato de email válido.' });
-            }
-            else if(error.code === 'auth/user-not-found') {
-                this.setState({ status: 'Esse email não pertence a nenhum usuário cadastrado.' });
-            }
-            else if(error.code === 'auth/wrong-password') {
-                this.setState({ status: 'A senha está incorreta.' });
-            }
-        });
+                if (user.user.photoURL == 'user') {
+                    this.props.navigation.navigate('UserHome');
+                }
+                else if (user.user.photoURL == 'admin') {
+                    this.props.navigation.navigate('AdminHome');
+                }
+
+                // Firebase.database().ref('/users/').once('value')
+                // .then((snapshot) => {
+                //     console.log(JSON.stringify(snapshot));
+                //     snapshot.val().map((data) => {
+                //         console.log(JSON.stringify(snapshot));
+                //         // if(data.email == user.email) {
+                //         //     if(data.bond == 'user') {
+                //         //         this.props.navigation.navigate('UserHome');
+                //         //     }
+                //         //     else if(data.bond == 'admin') {
+                //         //         this.props.navigation.navigate('AdminHome');
+                //         //     }
+                //         // }
+                //     });
+                // });
+            })
+            .catch((error) => {
+                if (error.code === 'auth/invalid-email') {
+                    this.setState({ status: 'Esse não é um formato de email válido.' });
+                }
+                else if (error.code === 'auth/user-not-found') {
+                    this.setState({ status: 'Esse email não pertence a nenhum usuário cadastrado.' });
+                }
+                else if (error.code === 'auth/wrong-password') {
+                    this.setState({ status: 'A senha está incorreta.' });
+                }
+            });
     }
 
     render() {
         return (
             <View style={styles.container}>
-                <Text>Faça Seu Login</Text>
 
-                <TextInput 
-                    placeholder='E-Mail'  
-                    value={ this.state.email }
-                    onChangeText={ (email) => this.setState({ email }) }
+                <Text style={styles.titleText}>Faça Seu Login</Text>
+
+                <TextInput
+
+                    style={{ height: 40, borderColor: 'white', borderWidth: 20, justifyContent: 'center', }}
+
+                    placeholder='E-Mail'
+                    value={this.state.email}
+                    onChangeText={(email) => this.setState({ email })}
                     keyboardType='email-address'
                     textContentType='emailAddress'
-                    autoCapitalize='none' /> 
+                    autoCapitalize='none' />
 
-                <TextInput 
-                    placeholder='Senha'  
-                    value={ this.state.password }
-                    onChangeText={ (password) => { this.setState({ password }) }}
+                <TextInput
+
+                    style={{ height: 40, borderColor: 'white', borderWidth: 20, justifyContent: 'center', }}
+
+                    placeholder='Senha'
+                    value={this.state.password}
+                    onChangeText={(password) => { this.setState({ password }) }}
                     secureTextEntry />
 
-                <Text>{ this.state.status }</Text>
+                <Text>{this.state.status}</Text>
 
-                <View>
-                    <Button 
-                        title='Entrar' 
-                        onPress={ () => {this.login()} } />
+                <View style={styles.fixToText}>
+                    <Button
+                        icon={{
+                            name: "arrow-right",
+                            size: 15,
+                            color: "white"
+                        }}
+                        title='Entrar'
+                        onPress={() => { this.login() }} />
                 </View>
             </View>
         );
@@ -96,5 +108,21 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
+    },
+
+    title: {
+        textAlign: 'center',
+        marginVertical: 8,
+    },
+
+    titleText: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        justifyContent: 'center',
+    },
+
+    fixToText: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
     },
 });
